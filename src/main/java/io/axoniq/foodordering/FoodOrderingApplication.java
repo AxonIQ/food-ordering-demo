5db6bf9a72be7e5dbf9ee3e1b2d0452c398ea568
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-03-25 13:33:45 +0100

    Add main class
    Add the FoodOrderingApplication, set up as a Spring Boot application

---
+++ src/main/java/io/axoniq/foodordering/FoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package io.axoniq.foodordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingApplication.class, args);
+	}
+
+}
