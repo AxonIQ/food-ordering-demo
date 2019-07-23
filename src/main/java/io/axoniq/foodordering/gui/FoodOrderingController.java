330cbb0496e9217c90dbcfa2032f340f88cc630a
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-07-23 13:51:03 +0200

    Make FoodOrderingController package-private
    Make the FoodOrderingController package-private to enforce segregation
    
    #5

--- src/main/java/io/axoniq/foodordering/gui/FoodOrderingController.java
+++ src/main/java/io/axoniq/foodordering/gui/FoodOrderingController.java
@@ -15,7 +15,7 @@ import java.util.concurrent.CompletableFuture;
 
 @RequestMapping("/foodCart")
 @RestController
-public class FoodOrderingController {
+class FoodOrderingController {
 
     private final CommandGateway commandGateway;
     private final QueryGateway queryGateway;
