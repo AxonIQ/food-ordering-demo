65921e1c93fcfa47def229711cf249f840d7220c
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-07-23 13:45:14 +0200

    Fill in FoodOrderingController
    Provide a food ordering rest controller, which utilizes the command and
    query gateway to dispatch commands/queries which interact with the
    FoodCart aggregate and to find a single FoodCartView
    
    #5

---
+++ src/main/java/io/axoniq/foodordering/gui/FoodOrderingController.java
@@ -0,0 +1,58 @@
+package io.axoniq.foodordering.gui;
+
+import io.axoniq.foodordering.coreapi.CreateFoodCartCommand;
+import io.axoniq.foodordering.coreapi.DeselectProductCommand;
+import io.axoniq.foodordering.coreapi.FindFoodCartQuery;
+import io.axoniq.foodordering.coreapi.SelectProductCommand;
+import io.axoniq.foodordering.query.FoodCartView;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@RequestMapping("/foodCart")
+@RestController
+public class FoodOrderingController {
+
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
+
+    public FoodOrderingController(CommandGateway commandGateway, QueryGateway queryGateway) {
+        this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
+    }
+
+    @PostMapping("/create")
+    public CompletableFuture<UUID> createFoodCart() {
+        return commandGateway.send(new CreateFoodCartCommand());
+    }
+
+    @PostMapping("/{foodCartId}/select/{productId}/quantity/{quantity}")
+    public void selectProduct(@PathVariable("foodCartId") String foodCartId,
+                              @PathVariable("productId") String productId,
+                              @PathVariable("quantity") Integer quantity) {
+        commandGateway.send(new SelectProductCommand(
+                UUID.fromString(foodCartId), UUID.fromString(productId), quantity
+        ));
+    }
+
+    @PostMapping("/{foodCartId}/deselect/{productId}/quantity/{quantity}")
+    public void deselectProduct(@PathVariable("foodCartId") String foodCartId,
+                                @PathVariable("productId") String productId,
+                                @PathVariable("quantity") Integer quantity) {
+        commandGateway.send(new DeselectProductCommand(
+                UUID.fromString(foodCartId), UUID.fromString(productId), quantity
+        ));
+    }
+
+    @GetMapping("/{foodCartId}")
+    public CompletableFuture<FoodCartView> findFoodCart(@PathVariable("foodCartId") String foodCartId) {
+        return queryGateway.query(
+                new FindFoodCartQuery(UUID.fromString(foodCartId)),
+                ResponseTypes.instanceOf(FoodCartView.class)
+        );
+    }
+}
