c23a4842a8c32ab9119439081df92e68081364bb
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-08-20 10:35:35 +0200

    Add @RoutingKey
    Add @RoutingKey so that this command can be routed by the
    AnnotationRoutingStrategy used in a default connection with Axon Server.
    
    #2

--- src/main/java/io/axoniq/foodordering/coreapi/commands.kt
+++ src/main/java/io/axoniq/foodordering/coreapi/commands.kt
@@ -1,9 +1,12 @@
 package io.axoniq.foodordering.coreapi
 
+import org.axonframework.commandhandling.RoutingKey
 import org.axonframework.modelling.command.TargetAggregateIdentifier
 import java.util.*
 
-class CreateFoodCartCommand
+class CreateFoodCartCommand(
+        @RoutingKey val foodCartId: UUID
+)
 
 data class SelectProductCommand(
         @TargetAggregateIdentifier val foodCartId: UUID,
