a04f1b23a52ea4016866260139f76705ccc86dbc
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-08-20 10:36:50 +0200

    Use provided foodCartId as aggregate id
    Use the provided foodCardId in the CreateFoodCardCommand as the
    aggregate id
    
    #3

--- src/main/java/io/axoniq/foodordering/command/FoodCart.java
+++ src/main/java/io/axoniq/foodordering/command/FoodCart.java
@@ -1,6 +1,14 @@
 package io.axoniq.foodordering.command;
 
-import io.axoniq.foodordering.coreapi.*;
+import io.axoniq.foodordering.coreapi.ConfirmOrderCommand;
+import io.axoniq.foodordering.coreapi.CreateFoodCartCommand;
+import io.axoniq.foodordering.coreapi.DeselectProductCommand;
+import io.axoniq.foodordering.coreapi.FoodCartCreatedEvent;
+import io.axoniq.foodordering.coreapi.OrderConfirmedEvent;
+import io.axoniq.foodordering.coreapi.ProductDeselectedEvent;
+import io.axoniq.foodordering.coreapi.ProductDeselectionException;
+import io.axoniq.foodordering.coreapi.ProductSelectedEvent;
+import io.axoniq.foodordering.coreapi.SelectProductCommand;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
@@ -29,8 +37,7 @@ public class FoodCart {
 
     @CommandHandler
     public FoodCart(CreateFoodCartCommand command) {
-        UUID aggregateId = UUID.randomUUID();
-        AggregateLifecycle.apply(new FoodCartCreatedEvent(aggregateId));
+        AggregateLifecycle.apply(new FoodCartCreatedEvent(command.getFoodCartId()));
     }
 
     @CommandHandler
@@ -91,5 +98,4 @@ public class FoodCart {
     public void on(OrderConfirmedEvent event) {
         confirmed = true;
     }
-
 }
